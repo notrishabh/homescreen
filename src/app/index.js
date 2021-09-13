9e518bc3b9482df3bf54bfa568e5434375c8774d
notrishabh <rishabh107107@gmail.com>
2021-09-13 06:00:25 +0530

    settings icon in app.js and settings component

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "../components/todo/Todo";
 import EpisodeCounter from "../components/episodeCounter/EpisodeCounter";
 import Quote from "../components/quotes/Quote";
+import Settings from "../components/Settings";
 import "../style/index.css";
 
 const App = () => {
+  const [openSettings, setOpenSettings] = useState(false);
+  const handleSettings = () => {
+    setOpenSettings(!openSettings);
+  };
   return (
-    <div className="h-screen bg-lol bg-cover p-5 overflow-hidden">
+    <div className="h-screen p-5 overflow-hidden bg-cover bg-lol">
+      <div>
+        <button
+          className="float-right p-1 bg-blue-400 bg-opacity-0 rounded-2xl hover:bg-opacity-100 hover:text-gray-300"
+          onClick={handleSettings}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        </button>
+      </div>
+      {openSettings ? <Settings /> : ""}
       <Quote />
       <Todo />
       <EpisodeCounter />
